7dc2ee4e8a1423b42af2aba06a6c7b2f7a741422
Doron Sionov <sionovd@post.bgu.ac.il>
2018-01-03 15:20:57 +0200

    the server tweets each weather update it receives

--- pubsub_subscriber.py
+++ pubsub_subscriber.py
@@ -12,18 +12,40 @@
 
 import time
 import os
+import tweepy  
+import sys
+import ast
 
 from google.cloud import pubsub
 from oauth2client.service_account import ServiceAccountCredentials
 
-project_id = 'awesome-project-31'  # Enter your project ID here
-topic_name = 'my_device_events'  # Enter your topic name here
+project_id = 'iot-weather-project'  # Enter your project ID here
+topic_name = 'topic'  # Enter your topic name here
 subscription_name = 'my_subscription'  # Can be whatever, but must be unique (for the topic?)
 service_account_json = 'service_account.json' # Location of the server service account credential file
 
+# Consumer keys and access tokens, used for OAuth  
+consumer_key = 'GsjokoqTGBv4JiJGzFV39djUG'  
+consumer_secret = 'ajq1ukXmZpa09gul1HWKnCeaiQ6AIw0vnSSW0SxPpWLIU3Jqu4'  
+access_token = '937304494202867712-cEXfYE3huKFatlijRTPh9uisOusDwpb'  
+access_token_secret = 'qivv8xozzby4N2XIYhdRrY64e6s5gsj5PcVRLQutDGZPk'
+
+# OAuth process, using the keys and tokens  
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)  
+auth.set_access_token(access_token, access_token_secret)
+   
+# Creation of the actual interface, using authentication  
+api = tweepy.API(auth)
 
 def on_message(message):
     """Called when a message is received"""
+    if message.data != 'PING':
+    	data = ast.literal_eval(message.data)
+    	tweet_text = "Time: %s\nTemperature: %s\nHumidity: %s\nPressure: %s\n" % (data[0], str(data[1]), str(data[2]), str(data[3]))
+        if len(tweet_text) <= 280:
+            api.update_status(status=tweet_text)
+        else:
+			print("The following tweet was too long to publish: ", tweet_text)
     print('Received message: {}'.format(message))
     message.ack()
 
